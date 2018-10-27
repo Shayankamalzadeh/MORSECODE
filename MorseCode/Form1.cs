8b5c7f7c690218338f88b9934d720561f03a70e3
shayan <shayan@DESKTOP-IDN6F5Q>
2018-10-27 18:02:13 +0330

    Add project files.

---
+++ MorseCode/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MorseCode
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<char, string> translator = new Dictionary<char, string>();
+        private char dot = '.';
+        private char dash = '−';
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            translator = new Dictionary<char, string>()
+            {
+                {'a', string.Concat(dot, dash)},
+                {'b', string.Concat(dash, dot, dot, dot)},
+                {'c', string.Concat(dash, dot, dash, dot)},
+                {'d', string.Concat(dash, dot, dot)},
+                {'e', dot.ToString()},
+                {'f', string.Concat(dot, dot, dash, dot)},
+                {'g', string.Concat(dash, dash, dot)},
+                {'h', string.Concat(dot, dot, dot, dot)},
+                {'i', string.Concat(dot, dot)},
+                {'j', string.Concat(dot, dash, dash, dash)},
+                {'k', string.Concat(dash, dot, dash)},
+                {'l', string.Concat(dot, dash, dot, dot)},
+                {'m', string.Concat(dash, dash)},
+                {'n', string.Concat(dash, dot)},
+                {'o', string.Concat(dash, dash, dash)},
+                {'p', string.Concat(dot, dash, dash, dot)},
+                {'q', string.Concat(dash, dash, dot, dash)},
+                {'r', string.Concat(dot, dash, dot)},
+                {'s', string.Concat(dot, dot, dot)},
+                {'t', string.Concat(dash)},
+                {'u', string.Concat(dot, dot, dash)},
+                {'v', string.Concat(dot, dot, dot, dash)},
+                {'w', string.Concat(dot, dash, dash)},
+                {'x', string.Concat(dash, dot, dot, dash)},
+                {'y', string.Concat(dash, dot, dash, dash)},
+                {'z', string.Concat(dash, dash, dot, dot)},
+                {'0', string.Concat(dash, dash, dash, dash, dash)},
+                {'1', string.Concat(dot, dash, dash, dash, dash)},  
+                {'2', string.Concat(dot, dot, dash, dash, dash)},
+                {'3', string.Concat(dot, dot, dot, dash, dash)},
+                {'4', string.Concat(dot, dot, dot, dot, dash)},
+                {'5', string.Concat(dot, dot, dot, dot, dot)},
+                {'6', string.Concat(dash, dot, dot, dot, dot)},
+                {'7', string.Concat(dash, dash, dot, dot, dot)},
+                {'8', string.Concat(dash, dash, dash, dot, dot)},
+                {'9', string.Concat(dash, dash, dash, dash, dot)}
+            };
+            var a = translateToMorse(textBox1.Text);
+            
+            translatetobeep(a);
+
+            // Console.Beep(5000, 1000);
+        }
+
+        private static string translateToMorse(string input)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (char character in input.ToLower())
+            {
+                if (translator.ContainsKey(character))
+                {
+                    sb.Append(translator[character] + " ");
+                }
+                else if (character == ' ')
+                {
+                    sb.Append("/ ");
+                }
+                else
+                {
+                    sb.Append(character + " ");
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void translatetobeep(string morsecode)
+        {
+            foreach (char character in morsecode)
+            {
+                textBox2.Text += character.ToString();
+                if (character == dot)
+                {
+                    Console.Beep(5000, 500);
+                }
+                else if (character == dash)
+                {
+                    Console.Beep(5000, 1000);
+                }
+
+            }
+        }
+    }
+}
